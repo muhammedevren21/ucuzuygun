264ef3d2541564792f36795e0e0166dfbb708b9f
Mac <macbook@Mahsun-MacBook-Pro.local>
2026-08-13 19:28:32 +0300

    middleware disable

--- middleware.ts
+++ middleware.ts
@@ -2,17 +2,9 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export function middleware(req: NextRequest) {
-  const token = req.cookies.get('sb-access-token') || req.cookies.get('sb-refresh-token')
-
-  if (req.nextUrl.pathname.startsWith('/satici/panel')) {
-    if (!token) {
-      return NextResponse.redirect(new URL('/satici/giris', req.url))
-    }
-  }
-
   return NextResponse.next()
 }
 
 export const config = {
-  matcher: ['/satici/panel/:path*'],
+  matcher: [],
 }
